532f7252b90a0cdee1996fbf1a99125ff321d8b3
chenyafe052 <chen.yafe052@gmail.com>
2019-01-23 20:30:33 +0200

    indent all

--- models/place.js
+++ models/place.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const placesSchema = new Schema({
     placeType:          { type: String, required: true},
-    placeGoogleId:      { type: String, required: true },
-    amountOfSelections:  { type: Number, required: true }
+    placeGoogleId:      { type: String, required: true},
+    amountOfSelections: { type: Number, required: true}
 });
 
 const placesModel = mongoose.model('place', placesSchema);
--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const morgan = require('morgan')
 //trip-planner modules import
 //const extremeCtrl = require('./controllers/extremeTripCtrl') ***Keep in Comment until remove google Maps import
 const sunBathCtrl = require('./controllers/sunBathController')
-const familyTripController = require('./controllers/familyTripCtrl')
+const familyTripCtrl = require('./controllers/familyTripCtrl')
 const placeCtrl = require('./controllers/placeCtrl')
-//const extremeController = require('./controllers/extremeTripCtrl') ***Keep in Comment until remove google Maps import
-
-
 const tripCtrlr = require('./controllers/tripCtrl')
 const userCtrl = require('./controllers/userCtrl')
+
 //server app uses
 const app = express()
 const port = process.env.PORT || 8000;
@@ -29,32 +27,30 @@ app.put('/api/newPlace/:placeGoogleId', placeCtrl.saveNewPlace)
 app.put('/api/update/:placeGoogleId', placeCtrl.updateAmount)
 
 /*User Routes HEN*/
-
 app.get('/api/addUser',userCtrl.addUser)
 
-/*Family Trip Routes SERGEI*/
-//app.get('/api/family/kidsAttraction',familyTripController.getKidsAttraction);
+/*Family Trip Routes */
+//app.get('/api/family/kidsAttraction',familyTripCntrl.getKidsAttraction);
 // app.get('/api/family/museum',()=>{});
 // app.get('/api/family/amusementPark',()=>{});
 // app.get('/api/family/shopping',()=>{});
 // app.get('/api/family/restrount',()=>{});
 
-/*SunBath Trip Routes HEN*/
+/* SunBath Trip Routes */
 app.get('/api/testbeach',sunBathCtrl.getBeaches);
 // app.get('/api/testspa',sunBathCtrl.getSpa);
 // app.get('/api/testshops',sunBathCtrl.getShops);
 // app.get('/api/testbars',sunBathCtrl.getBars);
 // app.get('/api/testres',sunBathCtrl.getRestaurant);
 
+/* Extreme Trip Routes */
+// app.get('/api/extreme/amusementPark',extremeCtrl.getParks)
+// app.get('/api/extreme/seaSport',extremeCtrl.getSeaSport)
+// app.get('/api/extreme/mount',extremeCtrl.getMount)
+// app.get('/api/extreme/ski',extremeCtrl.getSki)
+// app.get('/api/extreme/rv',extremeCtrl.getRv)
 
-/* Extreme Trip Routes CHEN*/
-// app.get('/api/extreme/amusementPark',asyncWrapper(extremeCtrl.getParks))
-// app.get('/api/extreme/seaSport',asyncWrapper(extremeCtrl.getSeaSport))
-// app.get('/api/extreme/mount',asyncWrapper(extremeCtrl.getMount))
-// app.get('/api/extreme/ski',asyncWrapper(extremeCtrl.getSki))
-// app.get('/api/extreme/rv',asyncWrapper(extremeCtrl.getRv))
-
-/*DATA BASE ROUTES $$change ()=>{}$$*/
+/* DATA BASE ROUTES */
 app.get('/api/db/trips', tripCtrlr.getAllTrips);
 app.get('/api/db/tripbyid:id',tripCtrlr.findTripByID)
 app.put('/api/db/editTrip:id',tripCtrlr.editTripByID);
