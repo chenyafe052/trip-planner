15ba04dafbe4a68a690725e8beaad38974581e02
chenyafe052 <chen.yafe052@gmail.com>
2019-01-24 03:44:31 +0200

    final backend

--- controllers/tripCtrl.js
+++ controllers/tripCtrl.js
@@ -62,49 +62,4 @@ module.exports = {
             res.status(404).send({ "error": "wrong params or not found" })
         }
     }
-    // setNewTrip(req, res, next) {
-    //     const { userId = null, tripType = null } = req.params
-    //     user_ID=userId
-    //     trip_type=tripType
-    //     const newTrip = new Trips({user_ID, trip_type})
-    //     newTrip.save(
-    //         (err) => {
-    //             if (result) res.json(result)
-    //             else res.status(404).send('not found')
-    //         })
-    // },
-    // //READ all trips
-    // async getAllTrips(req, res, next) {
-    //     const result = await tripModel.find({})
-    //     console.log(result);
-    //     if (result) res.status(200).json(result)
-    //     else res.status(404).send('not found')
-    // },
-    // //READ trip by tripId
-    // getTripById(req, res, next) {
-    //     const { tripId = null } = req.params
-    //     const result = Trips.find({ tripId })
-
-    //     if (result) res.json(result)
-    //     else res.status(404).send('not found')
-    // },
-    // //UPDATE new trip
-    // editTrip(req, res, next) {
-    //     const { tripId = null, userId = null } = req.params
-    //     const { tripType = null } = req.body
-    //     const result = Trips.find({ tripId })
-    //     result.save(
-    //         (err) => {
-    //             if (result) res.json(result)
-    //             else res.status(404).send('not found')
-    //     })
-    // },
-    // //DELETE trip by id
-    // deleteTrip(req, res, next) {
-    //     const {tripId = null } = req.params
-    //     Trips.deleteOne(tripId, (err, result) => {
-    //         if (result) res.json(result)
-    //         else res.status(404).send('not found')
-    //     })   
-    // }
 }
--- models/trip.js
+++ models/trip.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-    userId: { type: String, required: true },
+    userId:             { type: String, required: true },
     tripDateOfCreation: { type: String, required: true },
-    tripType: { type: String, required: true },
-    places: [String],
-    cancellationHistory: [String]
+    tripType:           { type: String, required: true },
+    places:             [String],
+    cancellationHistory:[String]
 });
 
 const tripsModel = mongoose.model('trip', tripSchema);
