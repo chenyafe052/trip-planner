2d4e9569bf1b7ac1c1fe2563db1c31b1d47ef1cb
hen aharon <henaharon@se.shenkar.ac.il>
2019-01-24 05:39:54 +0200

    tryingToReloadConsts

---
+++ Consts.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    MLAB_URL: 'mongodb://ds161724.mlab.com:61724/trip-planner-db',
+    DB_USER: 'db_admin',
+    DB_PASS : '3PXu8hSbXy83sMd',
+    GOOGLE_PLACES_API_KEY : '&key=AIzaSyDx7HFaSFpuvT2BOeiMh4qqL-U2RaxY5fo',
+    GOOGLE_PLACES_URL_FORMAT : 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=',
+    RADIUS : '&radius='
+
+}
